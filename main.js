7cfd1234b895cde3502739cb41a5f9866c5c6515
Franco Martinez Carballo <franco.mcarballo@outlook.com>
2020-05-29 22:44:34 -0600

    Select an Image and Send it to Main Process with IPCRenderer | Catch the sent Image im Main Process with ipcMain | Image processed with imagemin imagemin-mozjpeg imagemin-pngquant slash

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+const os = require('os');
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const slash = require('slash');
 
 // Set ENV variable
 process.env.NODE_ENV = 'development';
@@ -85,6 +91,33 @@ const menu = [
       ]
     : []),
 ];
+
+ipcMain.on('image:minimize', (e, opts) => {
+  opts.dest = path.join(os.homedir(), 'imageshrink');
+  optimizeImage(opts);
+  console.log(opts);
+});
+
+async function optimizeImage({ imgPath, quality, dest }) {
+  try {
+    const pngQuality = quality / 100;
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminMozjpeg({ quality }),
+        imageminPngquant({
+          quality: [pngQuality, pngQuality],
+        }),
+      ],
+    });
+
+    console.log(files);
+
+    shell.openPath(dest);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // Finaliza cuando todas las ventanas estén cerradas.
 app.on('window-all-closed', () => {
